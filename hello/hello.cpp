c69645097a19bb7fb35f418c1561b54de53bcc6e
fald <felipeaugustolira2006@gmail.com>
2026-01-05 19:44:28 -0300

    Hello world! Using C++ 17.

---
+++ hello/hello.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <iostream>
+
+int main ()
+{
+    std::cout << "Hello world! From the terminal + text editor" << std::endl;
+    std::cout << "C++ version being used is: "<< __cplusplus << std::endl;
+    return 0;
+}
